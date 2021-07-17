28f67724f58bc6618b6615d65e0d35f4b9f9209f
Dion <53874292+dion-shushi@users.noreply.github.com>
2021-07-16 19:13:35 -0500

    Flixter part 2 changes

--- app/src/main/java/com/example/flixter/modles/Movie.java
+++ app/src/main/java/com/example/flixter/modles/Movie.java
@@ -5,23 +5,37 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.flixter.MainActivity.TAG;
-
+@Parcel
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // empty constructor needed by the parceler library
+    public Movie(){
+
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException{
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -47,4 +61,6 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public double getRating() { return rating; }
 }
